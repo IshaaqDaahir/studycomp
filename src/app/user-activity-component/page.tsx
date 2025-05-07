434738386b550f39da96f41f98e5233affec3fed
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-05-07 21:42:15 +0100

    First Commit

---
+++ src/app/user-activity-component/page.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import avatar from "../../../public/images/avatar.svg";
+import Link from "next/link";
+
+export default function UserActivityComponent() {
+    return(
+        <div className="activities">
+            <div className="activities__header">
+                <h2>Recent Activities</h2>
+            </div>
+            {/* {% for message in room_messages %} */}
+                <div className="activities__box">
+                <div className="activities__boxHeader roomListRoom__header">
+                    <Link href="/profile" className="roomListRoom__author">
+                        <div className="avatar avatar--small">
+                            <Image src={avatar} alt="Message User Avatar" />
+                        </div>
+                        <p>
+                            @UserName
+                            <span>Message created|time ago</span>
+                        </p>
+                    </Link>
+
+                    {/* {% if request.user == message.user %} */}
+                        <div className="roomListRoom__actions">
+                            <Link href="/delete">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                <title>remove</title>
+                                <path
+                                    d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
+                                ></path>
+                                </svg>
+                            </Link>
+                        </div>
+                    {/* {% endif %} */}
+                </div>
+                <div className="activities__boxContent">
+                    <p>replied to post “<Link href="/room">Room Name</Link>”</p>
+                    <div className="activities__boxRoomContent">
+                        Message Body
+                    </div>
+                </div>
+                </div>
+            {/* {% endfor %} */}
+        </div>
+    );
+}
