e378ddffee3e2d808d340aaf530dd655792566d9
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-05-23 15:48:51 +0100

    Remove Dropdown Script

--- public/js/script.js
+++ public/js/script.js
@@ -43,17 +43,6 @@ if (actionButtons) {
 }
 
 
-// Menu
-const dropdownMenu = document.querySelector(".dropdown-menu");
-const dropdownButton = document.querySelector(".dropdown-button");
-
-if (dropdownButton) {
-  dropdownButton.addEventListener("click", () => {
-    dropdownMenu.classList.toggle('show');
-  });
-}
-
-
 // Upload Image
 const photoInput = document.querySelector("#avatar");
 const photoPreview = document.querySelector("#preview-avatar");
@@ -64,6 +53,7 @@ if (photoInput)
       photoPreview.src = URL.createObjectURL(file);
     }
   };
+
 
 // Scroll to Bottom
 const conversationThread = document.querySelector(".room__box");
