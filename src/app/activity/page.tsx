30699663f2566157d73366fb8e90a8817c1e2390
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-05-24 11:51:35 +0100

    Update Delete Message Links

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -41,21 +41,19 @@ export default async function ActivityComponent({ messagesList, query }: Activit
                         </p>
                     </Link>
 
-                    {/* {% if request.user == message.user %} */}
-                        <div className="roomListRoom__actions">
-                            <Link href="/delete">
-                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                                <title>remove</title>
-                                <path
-                                    d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
-                                ></path>
-                                </svg>
-                            </Link>
-                        </div>
-                    {/* {% endif %} */}
+                    <div className="roomListRoom__actions">
+                        <Link href={`/delete-message/${message.id}/`}>
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                            <title>remove</title>
+                            <path
+                                d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
+                            ></path>
+                            </svg>
+                        </Link>
+                    </div>
                 </div>
                 <div className="activities__boxContent">
-                    <p>replied to post in “<Link href={`/room/${message.room.id}`}>{message.room.name}</Link>”</p>
+                    <p>replied to post in “<Link href={`/room/${message.room.id}/`}>{message.room.name}</Link>”</p>
                     <div className="activities__boxRoomContent">
                         {message.body}
                     </div>
@@ -80,18 +78,16 @@ export default async function ActivityComponent({ messagesList, query }: Activit
                             </p>
                         </Link>
 
-                        {/* {% if request.user == message.user %} */}
-                            <div className="roomListRoom__actions">
-                                <Link href="/delete">
-                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                                    <title>remove</title>
-                                    <path
-                                        d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
-                                    ></path>
-                                    </svg>
-                                </Link>
-                            </div>
-                        {/* {% endif %} */}
+                        <div className="roomListRoom__actions">
+                            <Link href={`/delete-message/${message.id}/`}>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                <title>remove</title>
+                                <path
+                                    d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
+                                ></path>
+                                </svg>
+                            </Link>
+                        </div>
                     </div>
                     <div className="activities__boxContent">
                         <p>replied to post in “<Link href={`/room/${message.room.id}`}>{message.room.name}</Link>”</p>
