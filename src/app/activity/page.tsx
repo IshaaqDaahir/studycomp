b799e228f7d64aa71f48c508046ab2cca1de5d5d
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-05-25 14:25:58 +0100

    Fix Indentation

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -21,84 +21,84 @@ import { formatDistanceToNow } from 'date-fns';
 export default async function ActivityComponent({ messagesList, query }: ActivityComponentProps) {
     const defaultMessages = await fetchFromDjango('api/messages/');
 
-  return (
-    <div className="activities">
-      <div className="activities__header">
-        <h2>{query ? `Activity for "${query}"` : 'Recent Activities'}</h2>
-      </div>
-      {query ? (
-        messagesList?.length > 0 ? (
-          messagesList.map(message => (
-            <div key={message.id} className="activities__box">
-                <div className="activities__boxHeader roomListRoom__header">
-                    <Link href={`/profile/${message.user.id}/`} className="roomListRoom__author">
-                        <div className="avatar avatar--small">
-                            <Image src={message.user.avatar} width={32} height={32} alt="Message User Avatar" />
-                        </div>
-                        <p>
-                            @{message.user.username}
-                            <span>{formatDistanceToNow(new Date(message.created))} ago</span>
-                        </p>
-                    </Link>
-
-                    <div className="roomListRoom__actions">
-                        <Link href={`/delete-message/${message.id}/`}>
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                            <title>remove</title>
-                            <path
-                                d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
-                            ></path>
-                            </svg>
-                        </Link>
-                    </div>
-                </div>
-                <div className="activities__boxContent">
-                    <p>replied to post in “<Link href={`/room/${message.room.id}/`}>{message.room.name}</Link>”</p>
-                    <div className="activities__boxRoomContent">
-                        {message.body}
-                    </div>
-                </div>
+    return (
+        <div className="activities">
+            <div className="activities__header">
+                <h2>{query ? `Activity for "${query}"` : 'Recent Activities'}</h2>
             </div>
-          ))
-        ) : (
-          <p>No activity found matching "{query}"</p>
-        )
-      ) : (
-        <div>
-            {defaultMessages.map((message: Message) => (
-                <div key={message.id} className="activities__box">
-                    <div className="activities__boxHeader roomListRoom__header">
-                        <Link href={`/profile/${message.user.id}/`} className="roomListRoom__author">
-                            <div className="avatar avatar--small">
-                                <Image src={message.user.avatar} width={32} height={32} alt="Message User Avatar" />
-                            </div>
-                            <p>
-                                @{message.user.username}
-                                <span>{formatDistanceToNow(new Date(message.created))} ago</span>
-                            </p>
-                        </Link>
-
-                        <div className="roomListRoom__actions">
-                            <Link href={`/delete-message/${message.id}/`}>
-                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                                <title>remove</title>
-                                <path
-                                    d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
-                                ></path>
-                                </svg>
+            {query ? (
+                messagesList?.length > 0 ? (
+                messagesList.map(message => (
+                    <div key={message.id} className="activities__box">
+                        <div className="activities__boxHeader roomListRoom__header">
+                            <Link href={`/profile/${message.user.id}/`} className="roomListRoom__author">
+                                <div className="avatar avatar--small">
+                                    <Image src={message.user.avatar} width={32} height={32} alt="Message User Avatar" />
+                                </div>
+                                <p>
+                                    @{message.user.username}
+                                    <span>{formatDistanceToNow(new Date(message.created))} ago</span>
+                                </p>
                             </Link>
+
+                            <div className="roomListRoom__actions">
+                                <Link href={`/delete-message/${message.id}/`}>
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                    <title>remove</title>
+                                    <path
+                                        d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
+                                    ></path>
+                                    </svg>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="activities__boxContent">
+                            <p>replied to post in “<Link href={`/room/${message.room.id}/`}>{message.room.name}</Link>”</p>
+                            <div className="activities__boxRoomContent">
+                                {message.body}
+                            </div>
                         </div>
                     </div>
-                    <div className="activities__boxContent">
-                        <p>replied to post in “<Link href={`/room/${message.room.id}`}>{message.room.name}</Link>”</p>
-                        <div className="activities__boxRoomContent">
-                            {message.body}
+                ))
+                ) : (
+                <p>No activity found matching "{query}"</p>
+                )
+            ) : (
+                <div>
+                    {defaultMessages.map((message: Message) => (
+                        <div key={message.id} className="activities__box">
+                            <div className="activities__boxHeader roomListRoom__header">
+                                <Link href={`/profile/${message.user.id}/`} className="roomListRoom__author">
+                                    <div className="avatar avatar--small">
+                                        <Image src={message.user.avatar} width={32} height={32} alt="Message User Avatar" />
+                                    </div>
+                                    <p>
+                                        @{message.user.username}
+                                        <span>{formatDistanceToNow(new Date(message.created))} ago</span>
+                                    </p>
+                                </Link>
+
+                                <div className="roomListRoom__actions">
+                                    <Link href={`/delete-message/${message.id}/`}>
+                                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                        <title>remove</title>
+                                        <path
+                                            d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
+                                        ></path>
+                                        </svg>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="activities__boxContent">
+                                <p>replied to post in “<Link href={`/room/${message.room.id}`}>{message.room.name}</Link>”</p>
+                                <div className="activities__boxRoomContent">
+                                    {message.body}
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
-            ))}
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
