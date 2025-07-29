3efd3cef8594bef480764d00e3e10c25cc9f08d2
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-07-29 17:41:41 +0100

    Delete unused imports

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { fetchFromDjango } from "@/lib/api";
 import { formatDistanceToNow } from 'date-fns';
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
 // Types Declaration
     type Message = {
