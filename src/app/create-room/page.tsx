dd8df0ce1e4407c3392939870ac5e9f242877093
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-08-06 13:40:08 +0100

    Fix type error

--- src/app/create-room/page.tsx
+++ src/app/create-room/page.tsx
@@ -5,9 +5,12 @@ export default async function CreateRoomComponent() {
   let topics = [];
   try {
     topics = await fetchFromDjango('api/topics/');
-  } catch (error: any) {
-    // Optionally log error or show a message
-    console.error('Error fetching topics:', error.message);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Error fetching topics:', error.message);
+    } else {
+      console.error('Error fetching topics:', error);
+    }
   }
   return <RoomFormComponent topics={topics} />;
 }
