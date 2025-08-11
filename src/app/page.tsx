55a94887080dddd5fdd5665211119333a9255714
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-08-11 15:37:08 +0100

    Add paginated response type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,6 +34,14 @@ type Topic = {
     name: string;
 };
 
+// Type for paginated response
+type PaginatedResponse<T> = {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: T[];
+};
+
 export default async function Dashboard({ searchParams }: HomePageProps) {
     let rooms: Room[] = [];
     let topics: Topic[] = [];
