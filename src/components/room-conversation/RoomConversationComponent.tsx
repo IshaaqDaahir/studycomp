61dd5f6123af448e563d88209749c1acee05a3d8
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-08-25 10:49:33 +0100

    Hide delete message link if user is not authenticated

--- src/components/room-conversation/RoomConversationComponent.tsx
+++ src/components/room-conversation/RoomConversationComponent.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { fetchFromDjango } from "@/lib/api";
 import { formatDistanceToNow } from 'date-fns';
 import avatar from "../../../public/images/avatar.svg";
+import AuthWrapper from "../AuthWrapper";
 
 // Types Declaration
     type CurrentRoomId = {
@@ -39,17 +40,18 @@ export default async function RoomConversationComponent({ currentRoomId }: Curre
                                 </Link>
                                 <span className="thread__date">{formatDistanceToNow(new Date(message.created))} ago</span>
                             </div>
-
-                            <Link href={`/delete-room-message/${message.id}`}>
-                                <div className="thread__delete">
-                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                                        <title>remove</title>
-                                        <path
-                                        d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
-                                        ></path>
-                                    </svg>
-                                </div>
-                            </Link>
+                            <AuthWrapper>
+                                <Link href={`/delete-room-message/${message.id}`}>
+                                    <div className="thread__delete">
+                                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                            <title>Delete Message</title>
+                                            <path
+                                            d="M27.314 6.019l-1.333-1.333-9.98 9.981-9.981-9.981-1.333 1.333 9.981 9.981-9.981 9.98 1.333 1.333 9.981-9.98 9.98 9.98 1.333-1.333-9.98-9.98 9.98-9.981z"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </Link>
+                            </AuthWrapper>
                         </div>
                         <div className="thread__details">
                             {message.body}
