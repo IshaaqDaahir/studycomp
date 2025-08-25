10fdf7d3b43c18460e64e010361945c0e6511dcf
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-08-25 10:26:12 +0100

    Hide Create Room button for unauthenticated users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Suspense } from 'react';
 import { fetchFromDjango } from "@/lib/api";
 import ActivityComponent from "../components/activity/ActivityComponent";
+import AuthWrapper from "@/components/AuthWrapper";
 
 export const dynamic = 'force-dynamic';
 
@@ -134,17 +135,19 @@ export default async function HomePage({ searchParams }: HomePageProps) {
                             {errorMsg && <span style={{ color: 'red' }}>{errorMsg}</span>}
                         </div>
 
-                        {!query && (
-                            <Link className="btn btn--main" href="/create-room">
-                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                                <title>add</title>
-                                <path
-                                    d="M16.943 0.943h-1.885v14.115h-14.115v1.885h14.115v14.115h1.885v-14.115h14.115v-1.885h-14.115v-14.115z"
-                                ></path>
-                                </svg>
-                                {rooms.length === 0 ? 'Create First Room' : 'Create Room'}
-                            </Link>
-                        )}
+                        <AuthWrapper fallback={!query ? <Link className="btn btn--main" href="/login">Login to Create Room</Link> : null}>
+                            {!query && (
+                                <Link className="btn btn--main" href="/create-room">
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                    <title>add</title>
+                                    <path
+                                        d="M16.943 0.943h-1.885v14.115h-14.115v1.885h14.115v14.115h1.885v-14.115h14.115v-1.885h-14.115v-14.115z"
+                                    ></path>
+                                    </svg>
+                                    {rooms.length === 0 ? 'Create First Room' : 'Create Room'}
+                                </Link>
+                            )}
+                        </AuthWrapper>
                     </div>
 
                     {/* Feed Component */}
